545972b143c2792253af0cf797b4265f92d085aa
saumyagupta2000 <54590544+saumyagupta2000@users.noreply.github.com>
2020-06-22 15:11:09 +0530

    Add files via upload

---
+++ logincheck.php
@@ -0,0 +1,33 @@
+<?php
+    session_start();
+    $con = mysqli_connect('localhost', 'root');
+    if($con){
+        echo "connection established";
+    }
+    else{
+        echo "no connection";
+    }
+
+    $db = mysqli_select_db($con, 'admindatabase');
+
+    if(isset($_POST['submit'])){
+        $username = $_POST['user'];
+        $password = $_POST['pass'];
+        
+        $sql = "select * from admintable where user = '$username' and
+                                               pass = '$password' ";
+        $query = mysqli_query($con, $sql);
+
+        $row = mysqli_num_rows($query);
+            if($row == 1){
+                echo "Login Sucessful";
+                $_SESSION['user'] = $username;
+                header('location:adminmainpage.php');
+            }
+            else{
+                echo "login failed";
+                header('location:adminlogin.php');
+            }
+        
+    }
+?>
